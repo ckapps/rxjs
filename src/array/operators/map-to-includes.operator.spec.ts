fa65f5fba10859d78fc0c104a4acc27915ef9992
christophka <chris@ckapp.net>
2020-09-21 00:08:08 +0200

    chore: add unit tests for array operators

---
+++ src/array/operators/map-to-includes.operator.spec.ts
@@ -0,0 +1,30 @@
+import { range } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
+import { mapToIncludes } from './map-to-includes.operator';
+
+describe('array/operators/map-to-includes', () => {
+  let testScheduler: TestScheduler;
+  const values = [0, 1, 'test', { key: 'value' }];
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('should return if element is included', () => {
+    testScheduler.run(({ expectObservable }) => {
+      const source$ = range(0, values.length).pipe(mapToIncludes(values));
+
+      const expectedMarble = '(defg|)';
+      const expectedValues = {
+        d: true,
+        e: true,
+        f: false,
+        g: false,
+      };
+      expectObservable(source$).toBe(expectedMarble, expectedValues);
+    });
+  });
+});
---
+++ src/array/operators/take-if-included.operator.spec.ts
@@ -0,0 +1,33 @@
+import { range } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
+import { takeIfIncluded } from './take-if-included.operator';
+
+describe('array/operators/take-if-included', () => {
+  let testScheduler: TestScheduler;
+  const values = [false, undefined, null, 0, 1, 'test', { key: 'value' }];
+  const expectedValues = {
+    a: values[0],
+    b: values[1],
+    c: values[2],
+    d: values[3],
+    e: values[4],
+    f: values[5],
+    g: values[6],
+  };
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('should filter if not included', () => {
+    testScheduler.run(({ expectObservable }) => {
+      const source$ = range(0, values.length + 3).pipe(takeIfIncluded(values));
+
+      const expectedMarble = '(de|)';
+      expectObservable(source$).toBe(expectedMarble, expectedValues);
+    });
+  });
+});
