47fd01c47ee10fed2f99bdba1238da533aa1d486
christophka <chris@ckapp.net>
2020-11-09 01:17:15 +0100

    chore(common): move common filters

--- src/common/operators/filtering/filter-is-defined.operator.ts
+++ src/common/operators/filtering/filter-is-defined.operator.ts
@@ -1,9 +1,9 @@
-import { OperatorFunction } from 'rxjs';
+import { MonoTypeOperatorFunction } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
 /**
  * Emits value when it is not `undefined`.
  */
-export function filterIsDefined<T>(): OperatorFunction<T, T> {
+export function filterIsDefined<T>(): MonoTypeOperatorFunction<T> {
   return filter(x => x !== undefined);
 }
