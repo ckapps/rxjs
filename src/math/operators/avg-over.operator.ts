e99ab7e1bdd50fdb092523afc97e34fcdf744e01
christophka <chris@ckapp.net>
2020-11-17 00:40:48 +0100

    docs(math): improve code docs for avg-over

--- src/math/operators/avg-over.operator.ts
+++ src/math/operators/avg-over.operator.ts
@@ -4,8 +4,10 @@ import { bufferCount } from 'rxjs/operators';
 import { avg } from './avg.operator';
 
 /**
- * Operator to get an average of an single
- * @param bufferSize The size over which an average is calculated
+ * Calculates the arithmetic mean over a number of
+ * the last values emitted by the source observable.
+ *
+ * @param bufferSize The number of values to collection
  */
 export function avgOver(bufferSize: number): OperatorFunction<number, number> {
   return next => next.pipe(bufferCount(bufferSize), avg());
