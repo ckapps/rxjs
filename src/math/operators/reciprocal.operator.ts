e213d041eb0c625d7b92f78769702529dfc1e015
christophka <chris@ckapp.net>
2020-11-17 00:40:17 +0100

    fix: improve typing

--- src/math/operators/reciprocal.operator.ts
+++ src/math/operators/reciprocal.operator.ts
@@ -1,10 +1,10 @@
-import { OperatorFunction } from 'rxjs';
+import { MonoTypeOperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 /**
  * @returns
  * Reciprocal for the given value
  */
-export function reciprocal(): OperatorFunction<number, number> {
+export function reciprocal(): MonoTypeOperatorFunction<number> {
   return map(x => 1 / x);
 }
