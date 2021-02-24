8e9310d4300b723e2a1b5fb9d431538e704a6e9a
christophka <chris@ckapp.net>
2021-02-24 20:39:46 +0100

    feat(array): add operator mapToSome

---
+++ src/array/operators/map-to-some.operator.spec.ts
@@ -0,0 +1,32 @@
+import { TestScheduler } from 'rxjs/testing';
+
+import { mapToSome } from './map-to-some.operator';
+
+describe('array/operators/map-to-every', () => {
+  let testScheduler: TestScheduler;
+  const values = {
+    a: [false, false],
+    b: [false, true],
+    c: [true, false],
+    d: [true, true],
+  };
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
+  });
+
+  it('should return true if some item matches', () => {
+    testScheduler.run(({ expectObservable, cold }) => {
+      const source$ = cold('(abcd|)', values).pipe(mapToSome(v => v === true));
+
+      const expectedMarble = '(abbb|)';
+      const expectedValues = {
+        a: false,
+        b: true,
+      };
+      expectObservable(source$).toBe(expectedMarble, expectedValues);
+    });
+  });
+});
